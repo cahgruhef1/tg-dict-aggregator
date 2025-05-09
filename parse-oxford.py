bfacb933c68e2785afc61ef8359d652bfde91d07
Your Name <you@example.com>
2025-05-09 16:34:26 +0300

    Rename files to Kebab Case

--- parse-oxford.py
+++ parse-oxford.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_oxford(word):
-    TEMPLATE_URL = 'https://www.oxfordlearnersdictionaries.com/us/definition/american_english/'
+def parse_oxford(word: str):
+    TEMPLATE_URL = "https://www.oxfordlearnersdictionaries.com/us/definition/american_english/"
     HEADERS = {'User-Agent': 'Mozilla/5.0'}
     request = TEMPLATE_URL + word
     response = requests.get(request, headers=HEADERS)
-    html = BeautifulSoup(response.text, 'html.parser')
-    definitions = html.find_all('span', 'def')
+    html = BeautifulSoup(response.text, "html.parser")
+    definitions = html.find_all("span", "def")
     if len(definitions) == 0:
-        return False
+        return "Word not found."
     definitions = [definition.contents for definition in definitions]
     definitions_clear = []
     for definition in definitions:
@@ -20,5 +20,9 @@ def parse_oxford(word):
                 definition_clear.append(str(elem))
             else:
                 definition_clear.append(elem.contents[0])
-        definitions_clear.append(''.join(definition_clear))
-    return definitions_clear
+        definitions_clear.append("".join(definition_clear))
+    return "; ".join(definitions_clear)
+
+
+if __name__ == "__main__":
+    print(parse_oxford(input()))
