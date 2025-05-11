4e79be36b0f608df9e71f4274d53d34b9ccd089f
Your Name <you@example.com>
2025-05-11 23:13:54 +0300

    Update dictaggbot.py

--- dictaggbot.py
+++ dictaggbot.py
@@ -31,18 +31,18 @@ users = {}
 
 @bot.message_handler(commands=["start"])
 def start(message):
-	bot.reply_to(message, "Hi there! This is Telegram Dictionary Aggregator – a bot that compiles information about words from online dictionaries in a user-friendly way. Type “/help” for a list of all commands.")
+	bot.send_message(message.chat.id, "Hi there! This is Telegram Dictionary Aggregator – a bot that compiles information about words from online dictionaries in a user-friendly way. Type “/help” for a list of all commands.")
 
 @bot.message_handler(commands=["help"])
 def get_help(message):
-    bot.reply_to(message, """Here is a list of the available commands:
+    bot.send_message(message.chat.id, """Here is a list of the available commands:
 1) /start – start the bot,
 2) /help – get a list of all available commands,
 3) /get_word – get the definition of a word from different dictionaries.""")
 
 @bot.message_handler(commands=["get_word"])
 def get_word(message):
-    bot.reply_to(message , "Enter a word to get its definitions:")
+    bot.send_message(message.chat.id, "Enter a word to get its definitions:")
     if message.chat.id not in users:
         users[message.chat.id] = {}
     bot.register_next_step_handler(message, get_word2)
@@ -51,7 +51,10 @@ def get_word2(message):
     w = message.text
     users[message.chat.id].update({w: Word(w)})
     users[message.chat.id][w].get_defs()
-    bot.reply_to(message, f"""Here is a list of all definitions:
+    if users[message.chat.id][w].defs == []:
+        bot.reply_to(message, "I don’t know this word. Enter another word.")
+    else:
+        bot.reply_to(message, f"""Here is a list of all definitions:
 {"\n— ".join(users[message.chat.id][w].defs)}""")
 
 @bot.message_handler(func=lambda message: True)
