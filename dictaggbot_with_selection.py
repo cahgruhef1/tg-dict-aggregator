28dee3ff1bcf0192d14fe7aa1048b42049f3fa45
jacoblvovski <105866849+jacoblvovski@users.noreply.github.com>
2025-05-21 23:32:08 +0300

    Update dictaggbot_with_selection.py

--- dictaggbot_with_selection.py
+++ dictaggbot_with_selection.py
@@ -25,7 +25,7 @@ class Word():
                 continue
 
 
-bot = telebot.TeleBot("8028388743:AAHg2hw5U8fSm9CAUQW251p4N1i02vfyK5g", parse_mode="HTML")
+bot = telebot.TeleBot("", parse_mode="HTML")
 users = {}
 dict_list = ["1. meriam-webster.com", "2. dictionary.com", "3. Oxford Learner's dictionary"]
 dict_list = '\n'.join(dict_list)
