893fea69053f3ee0f787d146f315efe05964ef9f
jacoblvovski <105866849+jacoblvovski@users.noreply.github.com>
2025-05-21 18:15:53 +0300

    Add files via upload

---
+++ test_get_word.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+def get_word_of_the_day(id, level):
+    REQUEST_URL = f'https://raw.githubusercontent.com/cahgruhef1/tg-dict-aggregator/refs/heads/main/{level}_word_list.json'
+    response = requests.get(REQUEST_URL)
+    data = json.loads(response.text)
+    keys = data.keys()
+    keys = [int(key) for key in keys]
+    max_id = max(keys)
+    if id <= max_id:
+        return data[str(id)]
+    return False
+
+
+if __name__ == '__main__':
+    print(get_word_of_the_day(8, 'b1'))
