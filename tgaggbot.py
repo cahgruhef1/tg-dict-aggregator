61a1512573a0960187a19c25de707044002bd8b5
jacoblvovski <105866849+jacoblvovski@users.noreply.github.com>
2025-05-21 18:16:19 +0300

    Add files via upload
    Bot with word of the day function included

---
+++ tgaggbot.py
@@ -0,0 +1,90 @@
+import telebot
+from parse_dicts import *
+from test_get_word import *
+import time
+
+
+class Word():
+
+    def __init__(self, word: str,
+                       defs: list = None,
+                       synonyms: list = None,
+                       antonyms: list = None,
+                       examples: list = None):
+        self.word = word
+        self.defs = [] if defs is None else defs
+        self.synonyms = [] if synonyms is None else synonyms
+        self.antonyms = [] if antonyms is None else antonyms
+        self.examples = [] if examples is None else examples
+
+    def get_defs(self):
+        for parse_func in [parse_dictionary_com,
+                           parse_merriam_webster,
+                           parse_oxford,
+                           parse_wiktionary]:
+            try:
+                self.defs.append(parse_func(self.word))
+            except KeyError:
+                continue
+
+
+bot = telebot.TeleBot("7933991416:AAGofZGQVe9Gl7GYVkvUaqH0OBS1X6TV6v4", parse_mode="HTML")
+users = {}
+
+@bot.message_handler(commands=["start"])
+def start(message):
+	bot.send_message(message.chat.id, "Hi there! This is Telegram Dictionary Aggregator – a bot that compiles information about words from online dictionaries in a user-friendly way. Type “/help” for a list of all commands.")
+
+@bot.message_handler(commands=["help"])
+def get_help(message):
+    bot.send_message(message.chat.id, """Here is a list of the available commands:
+1) /start – start the bot,
+2) /help – get a list of all available commands,
+3) /get_word – get the definition of a word from different dictionaries.""")
+
+@bot.message_handler(commands=["get_word"])
+def get_word(message):
+    bot.send_message(message.chat.id, "Enter a word to get its definitions:")
+    if message.chat.id not in users:
+        users[message.chat.id] = {}
+    bot.register_next_step_handler(message, get_word2)
+
+def get_word2(message):
+    w = message.text
+    users[message.chat.id].update({w: Word(w)})
+    users[message.chat.id][w].get_defs()
+    if users[message.chat.id][w].defs == []:
+        bot.reply_to(message, "I don’t know this word. Enter another word.")
+    else:
+        defs_joined = "\n— ".join(users[message.chat.id][w].defs)
+        bot.reply_to(message, f"""Here is a list of all definitions:
+{defs_joined}""")
+@bot.message_handler(commands=["subscribe_to_word_of_the_day"])
+def get_level(message):
+    bot.send_message(message.chat.id, 'Select a vocabulary level: a1, a2, b1, b2, c1')
+    if message.chat.id not in users:
+        users[message.chat.id] = {}
+    bot.register_next_step_handler(message, start_subscription)
+def start_subscription(message):
+    level = message.text
+    users[message.chat.id].setdefault('subscription', {'levels': [], 'subscription_status': True,
+                                                       'word_of_the_day_id': 1})
+    users[message.chat.id]['subscription']['levels'].append(level)
+    while users[message.chat.id]['subscription']['subscription_status']:
+        for level in users[message.chat.id]['subscription']['levels']:
+            word_of_the_day = get_word_of_the_day(users[message.chat.id]['subscription']['word_of_the_day_id'], level)
+            users[message.chat.id].update({word_of_the_day: Word(word_of_the_day)})
+            users[message.chat.id][word_of_the_day].get_defs()
+            defs_joined = "\n— ".join(users[message.chat.id][word_of_the_day].defs)
+            bot.send_message(message.chat.id, f"""Here is a list of all definitions:
+            {defs_joined}""", time.sleep(86400))
+@bot.message_handler(commands=["unsubscribe_to_word_of_the_day"])
+def unsubscribe(message):
+    users[message.chat.id]['subscription']['subscription_status'] = False
+    bot.send_message(message.chat.id, 'You have unsubscribed from word of the day.')
+@bot.message_handler(func=lambda message: True)
+def send_default_reply(message):
+	bot.reply_to(message, "Sorry, I don’t understand. Type “/help” for a list of all commands.")
+
+
+bot.infinity_polling()
